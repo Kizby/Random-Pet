f485e527dedc6408230ee9dd1f983e4839cf644c
Αθηνα 𒂗𒃶𒌌𒀭𒈾 Washington <18bitsofentropy+gitkraken@gmail.com>
2021-12-30 21:55:58 -0500

    Drop reference to obsolete CreateCharacter workflow

--- RandomPet.cs
+++ RandomPet.cs
@@ -1,28 +1,10 @@
-using System;
 using HarmonyLib;
 using XRL.CharacterBuilds.Qud;
 using XRL.Rules;
-using XRL.UI;
 using XRL.World;
 
 namespace XRL.RandomPet
 {
-    [HarmonyPatch(typeof(CreateCharacter), "GenerateCharacter")]
-    public static class OldWorkflow_Patch
-    {
-        static void Prefix()
-        {
-            var options = GameObjectFactory.Factory.GetBlueprintsWithTag("StartingPet");
-            if (0 == options.Count)
-            {
-                return;
-            }
-            var result = options[Stat.Random(0, options.Count - 1)];
-            CreateCharacter.Template.Pet = result.Name;
-            CreateCharacter.Template.PetName = result.DisplayName();
-        }
-    }
-
     [HarmonyPatch(typeof(QudCustomizeCharacterModuleData), MethodType.Constructor)]
     public static class Patch
     {
